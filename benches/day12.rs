fbed638df7ed029073661f9609c84d882db194f0
NicolasVidal <pro@nicolasvidal.fr>
2024-12-12 23:56:08 +0100

    day12 : fix typo in benchmark

--- benches/day12.rs
+++ benches/day12.rs
@@ -1,14 +1,14 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let input_string = include_str!("../input/2024/dayt12.txt");
+    let input_string = include_str!("../input/2024/day12.txt");
 
-    c.bench_function("dayt12 p1", |b| {
-        b.iter(|| rust_nrjv_aoc_2024::dayt12::part1(input_string))
+    c.bench_function("day12 p1", |b| {
+        b.iter(|| rust_nrjv_aoc_2024::day12::part1(input_string))
     });
-    c.bench_function("dayt12 p2", |b| {
+    c.bench_function("day12 p2", |b| {
         b.iter(|| {
-            rust_nrjv_aoc_2024::dayt12::part2(black_box(input_string));
+            rust_nrjv_aoc_2024::day12::part2(black_box(input_string));
         })
     });
 }
