85ec8907fbec1a1987be1fc9b3b565a86d9ec053
NicolasVidal <pro@nicolasvidal.fr>
2024-12-15 23:11:01 +0100

    day15 p1 bench corrected

--- benches/day15.rs
+++ benches/day15.rs
@@ -1,14 +1,14 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let input_string = include_str!("../input/2024/day14.txt");
+    let input_string = include_str!("../input/2024/day15.txt");
 
-    c.bench_function("day14 p1", |b| {
-        b.iter(|| rust_nrjv_aoc_2024::day14::part1(black_box(input_string)))
+    c.bench_function("day15 p1", |b| {
+        b.iter(|| rust_nrjv_aoc_2024::day15::part1(black_box(input_string)))
     });
-    c.bench_function("day14 p2", |b| {
+    c.bench_function("day15 p2", |b| {
         b.iter(|| {
-            rust_nrjv_aoc_2024::day14::part2(black_box(input_string));
+            rust_nrjv_aoc_2024::day15::part2(black_box(input_string));
         })
     });
 }
