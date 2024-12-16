f6b8e8d8f56d404c9cd348bda83fc2cd58762ffe
NicolasVidal <pro@nicolasvidal.fr>
2024-12-16 17:19:35 +0100

    day15 : p1 get_unchecked

--- src/day15.rs
+++ src/day15.rs
@@ -39,16 +39,16 @@ pub fn part1(input: &str) -> u64 {
 
             match value {
                 b'.' => {
-                    grid[line][col] = Cell::Empty;
+                    (unsafe {*grid.get_unchecked_mut(line).get_unchecked_mut(col) = Cell::Empty});
                 }
                 b'#' => {
-                    grid[line][col] = Cell::Wall;
+                    (unsafe {*grid.get_unchecked_mut(line).get_unchecked_mut(col) = Cell::Wall});
                 }
                 b'O' => {
-                    grid[line][col] = Cell::Box;
+                    (unsafe {*grid.get_unchecked_mut(line).get_unchecked_mut(col) = Cell::Box});
                 }
                 b'@' => {
-                    grid[line][col] = Cell::Robot;
+                    (unsafe {*grid.get_unchecked_mut(line).get_unchecked_mut(col) = Cell::Robot});
                     robot_pos = (line, col);
                 }
                 _ => unsafe { unreachable_unchecked() },
@@ -72,10 +72,10 @@ pub fn part1(input: &str) -> u64 {
                 let mut line = robot_pos.0 - 1;
                 let mut found = false;
                 loop {
-                    if grid[line][robot_pos.1] == Cell::Wall {
+                    if (unsafe {*grid.get_unchecked_mut(line).get_unchecked_mut(robot_pos.1)}) == Cell::Wall {
                         break;
                     }
-                    if grid[line][robot_pos.1] == Cell::Empty {
+                    if (unsafe {*grid.get_unchecked_mut(line).get_unchecked_mut(robot_pos.1)}) == Cell::Empty {
                         found = true;
                         break;
                     }
@@ -91,11 +91,11 @@ pub fn part1(input: &str) -> u64 {
 
                 while line != robot_pos.0 {
                     let next_pos = (line, robot_pos.1);
-                    grid[next_pos.0][next_pos.1] = grid[next_pos.0 + 1][next_pos.1];
+                    (unsafe {*grid.get_unchecked_mut(next_pos.0).get_unchecked_mut(next_pos.1) = *grid.get_unchecked_mut(next_pos.0 + 1).get_unchecked_mut(next_pos.1)});
                     line += 1;
                 }
 
-                grid[robot_pos.0][robot_pos.1] = Cell::Empty;
+                (unsafe {*grid.get_unchecked_mut(robot_pos.0).get_unchecked_mut(robot_pos.1) = Cell::Empty});
                 robot_pos = (robot_pos.0 - 1, robot_pos.1);
             }
             b'v' => {
@@ -103,10 +103,10 @@ pub fn part1(input: &str) -> u64 {
                 let mut line = robot_pos.0 + 1;
                 let mut found = false;
                 loop {
-                    if grid[line][robot_pos.1] == Cell::Wall {
+                    if (unsafe {*grid.get_unchecked_mut(line).get_unchecked_mut(robot_pos.1)}) == Cell::Wall {
                         break;
                     }
-                    if grid[line][robot_pos.1] == Cell::Empty {
+                    if (unsafe {*grid.get_unchecked_mut(line).get_unchecked_mut(robot_pos.1)}) == Cell::Empty {
                         found = true;
                         break;
                     }
@@ -124,11 +124,11 @@ pub fn part1(input: &str) -> u64 {
 
                 while line != robot_pos.0 {
                     let next_pos = (line, robot_pos.1);
-                    grid[next_pos.0][next_pos.1] = grid[next_pos.0 - 1][next_pos.1];
+                    (unsafe {*grid.get_unchecked_mut(next_pos.0).get_unchecked_mut(next_pos.1) = *grid.get_unchecked_mut(next_pos.0 - 1).get_unchecked_mut(next_pos.1)});
                     line -= 1;
                 }
 
-                grid[robot_pos.0][robot_pos.1] = Cell::Empty;
+                (unsafe {*grid.get_unchecked_mut(robot_pos.0).get_unchecked_mut(robot_pos.1) = Cell::Empty});
                 robot_pos = (robot_pos.0 + 1, robot_pos.1);
             }
             b'>' => {
@@ -136,10 +136,10 @@ pub fn part1(input: &str) -> u64 {
                 let mut col = robot_pos.1 + 1;
                 let mut found = false;
                 loop{
-                    if grid[robot_pos.0][col] == Cell::Wall {
+                    if unsafe {*grid.get_unchecked_mut(robot_pos.0).get_unchecked_mut(col)} == Cell::Wall {
                         break;
                     }
-                    if grid[robot_pos.0][col] == Cell::Empty {
+                    if unsafe {*grid.get_unchecked_mut(robot_pos.0).get_unchecked_mut(col)} == Cell::Empty {
                         found = true;
                         break;
                     }
@@ -157,11 +157,11 @@ pub fn part1(input: &str) -> u64 {
 
                 while col != robot_pos.1 {
                     let next_pos = (robot_pos.0, col);
-                    grid[next_pos.0][next_pos.1] = grid[next_pos.0][next_pos.1 - 1];
+                    (unsafe {*grid.get_unchecked_mut(next_pos.0).get_unchecked_mut(next_pos.1) = *grid.get_unchecked_mut(next_pos.0).get_unchecked_mut(next_pos.1 - 1)});
                     col -= 1;
                 }
 
-                grid[robot_pos.0][robot_pos.1] = Cell::Empty;
+                (unsafe {*grid.get_unchecked_mut(robot_pos.0).get_unchecked_mut(robot_pos.1) = Cell::Empty});
                 robot_pos = (robot_pos.0, robot_pos.1 + 1);
             }
             b'<' => {
@@ -169,10 +169,10 @@ pub fn part1(input: &str) -> u64 {
                 let mut col = robot_pos.1 - 1;
                 let mut found = false;
                 loop {
-                    if grid[robot_pos.0][col] == Cell::Wall {
+                    if unsafe {*grid.get_unchecked_mut(robot_pos.0).get_unchecked_mut(col)} == Cell::Wall {
                         break;
                     }
-                    if grid[robot_pos.0][col] == Cell::Empty {
+                    if unsafe {*grid.get_unchecked_mut(robot_pos.0).get_unchecked_mut(col)} == Cell::Empty {
                         found = true;
                         break;
                     }
@@ -190,11 +190,11 @@ pub fn part1(input: &str) -> u64 {
 
                 while col != robot_pos.1 {
                     let next_pos = (robot_pos.0, col);
-                    grid[next_pos.0][next_pos.1] = grid[next_pos.0][next_pos.1 + 1];
+                    (unsafe {*grid.get_unchecked_mut(next_pos.0).get_unchecked_mut(next_pos.1) = *grid.get_unchecked_mut(next_pos.0).get_unchecked_mut(next_pos.1 + 1)});
                     col += 1;
                 }
 
-                grid[robot_pos.0][robot_pos.1] = Cell::Empty;
+                (unsafe {*grid.get_unchecked_mut(robot_pos.0).get_unchecked_mut(robot_pos.1) = Cell::Empty});
                 robot_pos = (robot_pos.0, robot_pos.1 - 1);
             }
             _ => unsafe { unreachable_unchecked() },
@@ -220,7 +220,7 @@ pub fn part1(input: &str) -> u64 {
     let mut total = 0u64;
     for row in 0..ROWS {
         for col in 0..COLS {
-            if grid[row][col] == Cell::Box {
+            if unsafe {*grid.get_unchecked_mut(row).get_unchecked_mut(col) == Cell::Box} {
                 total += 100 * row as u64 + col as u64;
             }
         }
